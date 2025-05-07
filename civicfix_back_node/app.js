6931fbe22d32753f6791cfc31b107d76b9077d44
jason <firkus.jason@gmail.com>
2025-05-07 02:51:38 -0700

    create pool and connect to aiven civicfix db, begin MyIssues.jsx

--- civicfix_back_node/app.js
+++ civicfix_back_node/app.js
@@ -1,20 +1,35 @@
 import express, { json, static as expressStatic } from "express";
 import { createPool } from "mysql2/promise";
 import { readFileSync } from "fs";
-import dotenv from "dotenv";
+import "dotenv/config";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 
-dotenv.config();
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
+
+const pool = createPool({
+  host: process.env.AIVEN_HOST,
+  port: process.env.AIVEN_PORT,
+  user: process.env.AIVEN_USER,
+  password: process.env.AIVEN_PASSWORD,
+  database: process.env.AIVEN_DATABASE,
+  ssl: {
+    ca: readFileSync(__dirname + "/ca.pem"),
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
 app.use(json());
+// app.use(express.static(join(__dirname, "../civicfix_front_react_js/dist")));
 
-app.use(express.static(join(__dirname, "../civicfix_front_react_js/dist")));
-
-app.get("/", (req, res) => {
-  res.status(200).sendFile(join(__dirname, "../civicfix_front_react_js/dist", "index.html"));
+app.get("/issues", async (req, res) => {
+  const [issues] = await pool.query("SELECT id, title, type, status FROM issues WHERE user = ?;", [
+    req.session.userid,
+  ]);
+  res.status(200).json(issues);
 });
 
 app.listen(process.env.PORT, () => {
