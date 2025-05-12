6d1d2608bea91280fd01af4fcd1b6bbda333ca93
jason <firkus.jason@gmail.com>
2025-05-12 16:52:39 -0700

    add gen-description endpoint and connect gemini api

--- civicfix_back_node/app.js
+++ civicfix_back_node/app.js
@@ -4,6 +4,9 @@ import { readFileSync } from "fs";
 import "dotenv/config";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
@@ -15,6 +18,29 @@ app.use(expressStatic(join(__dirname, "../civicfix_front_react_js/dist")));
 
 app.get("/", (req, res) => {
   res.status(200).sendFile(join(__dirname, "../civicfix_front_react_js/dist", "index.html"));
+});
+
+app.get("/issues/gen-description", async (req, res) => {
+  try {
+    //TODO maybe add type and other parameters to increase description accuracy?
+    const { title, location } = req.query;
+
+    console.log("Generating description for title: ", title);
+    console.log("Generating description for location: ", location);
+
+    const { text: description } = await ai.models.generateContent({
+      model: "gemini-2.5-flash-preview-04-17",
+      contents: `In our local community issue reporting app, CivicFix, the user has entered a title for an issue they wish to report. Generate a 2-3 sentence description for this issue based on the provided issue title and issue location.
+        \nIssue title: ${title}
+        \nIssue location: ${location}`,
+    });
+
+    console.log("Generated description: ", description);
+    res.status(200).json({ description });
+  } catch (error) {
+    console.error("Error generating description", error);
+    res.status(500).json({ message: "Error generating description", error });
+  }
 });
 
 app.get("/location-data", async (req, res) => {
